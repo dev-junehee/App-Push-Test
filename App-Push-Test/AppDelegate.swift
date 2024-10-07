620a18b7e20870f4461a2da2892d0d5f8a594963
dev-junehee <dev.junehee@gmail.com>
2024-10-07 14:51:45 +0900

    1007 : App Push Notification (FCM - Firebase Cloud Messaging)

--- App-Push-Test/AppDelegate.swift
+++ App-Push-Test/AppDelegate.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        /// Firebase 등록
+        FirebaseApp.configure()
+        
+        /// 원격 알림 등록
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+
+        application.registerForRemoteNotifications()
+        
+        /// 메세지 대리자 설정. 등록 토큰을 수신하기 위해 필요!
+        Messaging.messaging().delegate = self
+        
         return true
     }
 
@@ -34,3 +53,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+/// Apple
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+}
+
+/// FCM
+extension AppDelegate: MessagingDelegate {
+    /// 현재 등록 토큰(디바이스마다 고유한 토큰 정보) 가져오기
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        /// 현재 등록 토큰 가져오기
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+            print("FCM registration token: \(token)")
+            // self.fcmRegTokenMessage.text  = "Remote FCM registration token: \(token)" /// fcmTokenMessage 뷰 객체가 없어서 오류나는 것! 삭제해도 된다.
+          }
+        }
+        
+        /// 토큰 갱신 모니터링 -> 이 로직이 왜 필요할까?
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+    }
+}
